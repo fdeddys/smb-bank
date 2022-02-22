a971f0cb93cc26abf6d4ea2ee182d4da96559624
deddy <fransiscus.deddy@gmail.com>
2022-02-23 06:06:30 +0700

    first commit

--- services/account.service.go
+++ services/account.service.go
@@ -11,18 +11,5 @@ func (a AccountService) GetAccount(id int) dto.ContentResponse {
 
 	var res dto.ContentResponse
 
-	// data, errCode, errDesc, err := repository.GetBrandFilter(id)
-
-	// if err != nil {
-	// 	res.Contents = nil
-	// 	res.ErrCode = "02"
-	// 	res.ErrDesc = "Error query data to DB"
-	// 	return res
-	// }
-
-	// res.Contents = data
-	// res.ErrCode = errCode
-	// res.ErrDesc = errDesc
-
 	return res
 }
